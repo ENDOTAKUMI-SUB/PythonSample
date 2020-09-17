7fde3d5945f0e317a01eeb05f5c41bbd838741e6
pc001 <en0605taku@gmail.com>
2020-09-17 09:52:10 +0900

    Commit

--- Chapter4/Ex4-4-19.py
+++ Chapter4/Ex4-4-19.py
@@ -1,13 +1,16 @@
+def log():
+    print('{} i:{} j:{} w:{}'.format(a,i,j,w))
+
 a = [40, 20, 50, 30, 10]
 i = 1
 while i <= 4:
     w = a[i]
     j = i - 1
     while j >= 0 and a[j] > w:
-        print('{} i:{} j:{} w:{}'.format(a,i,j,w))
+        log()
         a[j + 1] = a[j]
         j -= 1
-    print('{} i:{} j:{} w:{}'.format(a,i,j,w))
+    log()
     a[j + 1] = w
     i += 1
-print('{} i:{} j:{} w:{}'.format(a,i,j,w))
+log()
